6dea45768a83507a554723500b18540e30d2c387
Dudu Lacerda <dudu.lacerda.dl@gmail.com>
2023-11-11 15:57:26 -0300

    :sparkles: Alterado sprite do player e adicionado nivel 2

--- Assets/Scripts/Banana.cs
+++ Assets/Scripts/Banana.cs
@@ -8,6 +8,7 @@ public class Banana : MonoBehaviour
     private CircleCollider2D circle;
 
     public GameObject collected;
+    public int Score;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,9 @@ public class Banana : MonoBehaviour
             sr.enabled = false;
             circle.enabled = false;
             collected.SetActive(true);
+
+            GameController.instance.totalScore += Score;
+            GameController.instance.UpdateScoreText();
 
             Destroy(gameObject, 0.25f);
         }
